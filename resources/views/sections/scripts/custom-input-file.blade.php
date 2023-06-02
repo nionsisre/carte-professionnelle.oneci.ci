9453e4f1cfd9b50f33095b0ad67627accd61128a
Patrick N'DRI <patrickangel.ndri@gmail.com>
2023-06-02 14:24:38 +0000

    Prise en compte des symboles dans les noms des fichiers

--- resources/views/sections/scripts/custom-input-file.blade.php
+++ resources/views/sections/scripts/custom-input-file.blade.php
@@ -12,15 +12,22 @@ jQuery('.inputfile').each(function () {
     $input.on('change', function (e) {
         var fileName = '';
 
-        if (this.files && this.files.length > 1)
+        if (this.files && this.files.length > 1) {
             fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
-        else if (e.target.value)
-            fileName = e.target.value.split('\\').pop();
+        } else if (e.target.value) {
+            {{-- fileName = e.target.value.split('\\').pop(); --}}
+            {{-- Renommage du fichier en retirant les espaces et les caracteres speciaux --}}
+            fileName = e.target.value.split('\\').pop().replace(/['"[\]{}()<>+*=/\\|?:^~]/g, '').replace(/[^\w.-]/g, '-');
+        }
 
-        if (fileName)
+        if (fileName) {
             $label.find('span').html(fileName);
-        else
+        } else {
             $label.html(labelVal);
+        }
+
+        {{-- attribution de la valeur du fileName du fichier chargé dans le champ input du formulaire a soumettre --}}
+        $input.val(fileName);
     });
 
     {{-- Firefox bug fix --}}
