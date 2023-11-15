8d352784967f9099ad80579b367762c3c5593f6f
Patrick N'DRI <patrickangel.ndri@gmail.com>
2023-11-15 12:58:55 +0000

    implémentation get agence list backend ostatplus 2

--- app/Http/Controllers/OstatPlus/ReportController.php
+++ app/Http/Controllers/OstatPlus/ReportController.php
@@ -28,21 +28,12 @@ class ReportController extends Controller {
         $jsonData = $request->json()->all();
         /* Valider les variables de l'API */
         $validator = Validator::make($jsonData, [
-            'role_id' => ['required', 'string'], // Role id
-            'user_role_label' => ['required', 'string'], // Role Label
-            'zone_code' => ['required', 'string'], // Zone Code
+            'role_id' => ['required'], // Role id
+            'zone_code' => ['nullable', 'string'], // Zone Code
             'uid' => ['required', 'string'], // Uid
             'login' => ['required', 'string'], // Login
-            'status_id' => ['required', 'string'], // Status ID
-            'monitored' => ['required', 'string'], // isMonitored
-            'first_name' => ['required', 'string'], // First Name
-            'last_name' => ['required', 'string'], // Last Name
-            'phone_number' => ['required', 'string'], // Phone Number
-            'phone_number_2' => ['required', 'string'], // Phone Number 2
-            'phone_number_3' => ['required', 'string'], // Phone Number 3
-            'email' => ['required', 'string'], // Email
-            'creation_date' => ['required'], // Created At
-            'update_date' => ['required'], // Updated At
+            'status_id' => ['required'], // Status ID
+            'monitored' => ['required'], // isMonitored
         ]);
         if (!$validator->fails()) {
             /* Accéder aux variables soumises dans le corps JSON */
