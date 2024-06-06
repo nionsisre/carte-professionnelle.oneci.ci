fc3cc1c7a6a78a0c5881f605e8f06cc3cd87bbdf
Patrick N'DRI <patrickangel.ndri@gmail.com>
2024-06-06 20:38:25 +0000

    Prise en compte du numéro de téléphone et envoi de SMS 2

--- app/Http/Controllers/Admin/ProcessCertificatConformiteController.php
+++ app/Http/Controllers/Admin/ProcessCertificatConformiteController.php
@@ -205,12 +205,12 @@ class ProcessCertificatConformiteController extends Controller {
                 $centre = DB::connection(env('DB_CONNECTION_KERNEL'))->table('centre_unified')->where('code_unique_centre','=',$client->code_lieu_retrait)->first();
                 (new SMS)->sendSMS(
                     $client->msisdn,
-                    "M(Mme) ".$client->nom.", vos documents justificatifs ont été approuvés et votre certificat de conformité est présentement en attente de signature et d'acheminement à ".ucwords(strtolower($centre->location_label.', '.$centre->area_label.', '.$centre->department_label)),
+                    "M(Mme) ".$client->nom.", vos documents justificatifs de votre demande N°".$client->numero_dossier." ont été approuvés et votre certificat de conformité est présentement en attente de signature et d'acheminement à ".ucwords(strtolower($centre->location_label.', '.$centre->area_label.', '.$centre->department_label)),
                 );
             } else {
                 (new SMS)->sendSMS(
                     $client->msisdn,
-                    "M(Mme) ".$client->nom.", vos documents justificatifs ont été approuvés et votre certificat de conformité est présentement en attente de signature et d'acheminement dans votre centre de retrait.",
+                    "M(Mme) ".$client->nom.", vos documents justificatifs de votre demande N°".$client->numero_dossier." ont été approuvés et votre certificat de conformité est présentement en attente de signature et d'acheminement dans votre centre de retrait.",
                 );
             }
             $client->statut = 3;
@@ -236,12 +236,12 @@ class ProcessCertificatConformiteController extends Controller {
             if(!empty(request()->input('obs'))) {
                 (new SMS)->sendSMS(
                     $client->msisdn,
-                    "M(Mme) ".$client->nom.", votre demande de certificat de conformité a été rejetée pour le motif suivant : ".$request->input('obs'),
+                    "M(Mme) ".$client->nom.", votre demande de certificat de conformité N°".$client->numero_dossier." a été rejetée pour le motif suivant : ".$request->input('obs'),
                 );
             } else {
                 (new SMS)->sendSMS(
                     $client->msisdn,
-                    "M(Mme) ".$client->nom.", votre demande de certificat de conformité a été rejetée par l'ONECI",
+                    "M(Mme) ".$client->nom.", votre demande de certificat de conformité N°".$client->numero_dossier." a été rejetée par l'ONECI",
                 );
             }
             $client->statut = 4;
