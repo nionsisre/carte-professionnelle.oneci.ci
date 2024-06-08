725ddcff70776e32fea5ca31d4e616cb3b8f3a48
Patrick N'DRI <patrickangel.ndri@gmail.com>
2024-06-08 16:17:26 +0000

    Back Office : Ajout de la migration de suivi des actions de l'utilisateur

---
+++ database/migrations/2024_06_06_115810_create_user_activities_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_activities', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\User::class)->nullable();
+            $table->string('uuid')->nullable();
+            $table->string('action');
+            $table->string('details')->nullable();
+            $table->string('concerned_uuid')->nullable();
+            $table->string('impact_level');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_activities');
+    }
+}
