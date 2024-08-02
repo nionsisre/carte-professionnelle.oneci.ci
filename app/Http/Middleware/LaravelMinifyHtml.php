3d4fc30d5e4567ebec326d6e8aef8cbe0bdbaff8
Patrick N'DRI <patrickangel.ndri@gmail.com>
2024-08-02 10:25:12 +0000

    Suppression du middleware IsConneted
    Suppression du fichier middleware IsConneted obsolète. Ajout de l'exception 'check-status-payment' au middleware CSRF et réactivation de la minification HTML pour certains itinéraires.

--- app/Http/Middleware/LaravelMinifyHtml.php
+++ app/Http/Middleware/LaravelMinifyHtml.php
@@ -15,7 +15,7 @@ class LaravelMinifyHtml
         // check if environment is production
         if(env('APP_ENV') === "production") {
             if ($this->isResponseObject($response) && $this->isHtmlResponse($response) && Config::get('htmlminify.default')) {
-                if (!(Route::currentRouteName() == 'login' || Route::currentRouteName() == 'authentificaton' || Route::currentRouteName() == 'password.rest'
+                /*if (!(Route::currentRouteName() == 'login' || Route::currentRouteName() == 'authentificaton' || Route::currentRouteName() == 'password.rest'
                     || Route::currentRouteName() == 'oneci.update.password' || Route::currentRouteName() == 'abonnees.*' || Route::currentRouteName() == 'abonnees.*'
                     || Route::currentRouteName() == 'admin_home' || Route::currentRouteName() == 'rapport' || Route::currentRouteName() == 'setting'
                     || Route::currentRouteName() == 'user' || Route::currentRouteName() == 'add.user' || Route::currentRouteName() == 'update.user'
@@ -23,7 +23,8 @@ class LaravelMinifyHtml
                     || Route::currentRouteName() == 'rapport.import' || Route::currentRouteName() == 'abonnees.exportation' || Route::currentRouteName() == 'logout.importation'
                     || Route::currentRouteName() == 'abonnes.validation' || Route::currentRouteName() == 'abonnees.validation.search' || Route::currentRouteName() == 'abonnees.validation.update')) {
                     $response->setContent(LaravelHtmlMinifyFacade::htmlMinify($response->getContent()));
-                }
+                } */
+                $response->setContent(LaravelHtmlMinifyFacade::htmlMinify($response->getContent()));
             }
         }
         return $response;
